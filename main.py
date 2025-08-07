136c3f74f502e279c572741aaba56a0a45abf276
FreshFrie <163244064+FreshFrie@users.noreply.github.com>
2025-08-07 04:56:51 +0000

    s

---
+++ main.py
@@ -0,0 +1,3 @@
+name = input("What is your name: ")
+
+print(f"Your name is {name.capitalize()}, your name also starts with the character {name[0].capitalize()} and has {len(name)} characeters in it.")
